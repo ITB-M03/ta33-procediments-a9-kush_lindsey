73f25444d60e3b4f956685cbce1ccf90762d0ee5
Kush4-ES <khushbir.kumar.7e8@itb.cat>
2024-12-15 18:57:50 +0100

    Update billeteMetro.kt

--- src/main/kotlin/controllers/billeteMetro.kt
+++ src/main/kotlin/controllers/billeteMetro.kt
@@ -1,21 +1,50 @@
 package org.example.controllers
 import java.util.*
-const val billeteSenzillo : Float = 2.40f
-const val tcasual : Float = 11.35f
-const val tusual : Float = 40.00f
-const val tfamiliar : Float = 10.00f
-const val tjove : Float = 80.00f
+data class PrecioBilletes(
+    var zonas : String,
+    var billeteSenzillo : Float,
+    var tcasual : Float ,
+    var tusual : Float ,
+    var tfamiliar : Float ,
+    var tjove : Float
+)
 fun main() {
+    val billetes : Array<PrecioBilletes> = arrayOf(
+        PrecioBilletes(
+            "zona1",
+            2.40f,
+            11.35f,
+            40.00f,
+            10.00f,
+            80.00f
+        ),
+        PrecioBilletes(
+            "zona2",
+            2.40f*1.3125f,
+            11.35f*1.3125f,
+            40.00f*1.3125f,
+            10.00f*1.3125f,
+            80.00f*1.3125f
+        ),
+        PrecioBilletes(
+            "zona3",
+            2.40f*1.8443f,
+            11.35f*1.8443f,
+            40.00f*1.8443f,
+            10.00f*1.8443f,
+            80.00f*1.8443f
+        )
+    )
     val scan = inicioscan()
     val billete = tipoBillete(scan)
     val zona = tipoZona(scan)
-    val precio = costeBillete()
+    val precio = costeBillete(zona,billete,billetes)
     finalscan(scan)
 }
 fun inicioscan(): Scanner {
     return Scanner(System.`in`).useLocale(Locale.UK)
 }
-fun tipoBillete(scan: Scanner){
+fun tipoBillete(scan: Scanner):String{
     println("Quin bitllet desitja adquirir?")
     println("1- Bitllet Senzill")
     println("2 - TCasual")
@@ -23,31 +52,42 @@ fun tipoBillete(scan: Scanner){
     println("4 - TFamiliar")
     println("5 - TJove")
     val scan=Scanner(System.`in`)
-    val tipoBillete = scan.nextInt()
-    when(tipoBillete){
-        1-> tipoZona(scan)
-        2 -> tipoZona(scan)
-        3 ->tipoZona(scan)
-        4 -> tipoZona(scan)
-        5 -> tipoZona(scan)
-    }
+    val tipoBillete = scan.nextInt().toString()
+    scan.nextLine().lowercase()
+    return tipoBillete
 }
 
-fun tipoZona(scan: Scanner){
+fun tipoZona(scan: Scanner):String{
     println("Quina zona vol viatjar?")
     println("1")
     println("2")
     println("3")
     val scan=Scanner(System.`in`)
-    val tipoZona = scan.nextInt()
+    var tipoZona = scan.nextInt().toString()
+    return tipoZona
+}
+fun costeBillete(zonas: String, billete:String, billetes: Array<PrecioBilletes>){
+    var calculoPrecio = 0f
+    for (i in billetes.indices) {
+        if (zonas == "zona1" && billete == "billete senzillo" || billete == "tcasual" || billete == "tfamiliar" || billete == "tjove") {
 
+        } else if (zonas == "zona2" && billete == "billete senzillo" || billete == "tcasual" || billete == "tfamiliar" || billete == "tjove") {
+
+        } else if (zonas == "zona3" && billete == "billete senzillo" || billete == "tcasual" || billete == "tfamiliar" || billete == "tjove") {
+
+        }
+        else{
+            println("ERROR")
+        }
+        println(calculoPrecio)
+    }
+}
+fun mostrarTicket(){
 
 }
-fun costeBillete(){
+fun pagar(){
 
 }
-
-
 
 fun finalscan(scan: Scanner) {
     scan.close()
