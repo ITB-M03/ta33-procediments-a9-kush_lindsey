071ba1a54dd98bd8cb632ad6205e0c2ee98b76ca
Khushbir Kumar <khushbir.kumar.7e8@itb.cat>
2024-12-17 19:29:01 +0100

    K

---
+++ src/test/kotlin/controllers/BilleteMetroKtTest.kt
@@ -0,0 +1,27 @@
+package controllers
+
+import org.junit.jupiter.api.Test
+
+import org.junit.jupiter.api.Assertions.*
+
+class BilleteMetroKtTest {
+
+    @Test
+    fun tipoBillete() {
+        var expected = 1
+    }
+
+    @Test
+    fun tipoZona() {
+    }
+
+    @Test
+    fun calcularCoste() {
+    }
+
+    @Test
+    fun repetirProceso() {
+        var expectation = true
+        assertEquals(expected, repetirProceso(true))
+    }
+}
